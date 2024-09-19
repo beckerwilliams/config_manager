0f55e1893bf6e749de484d16461dcca098891568
austinRon <becker.wiliams@gmail.com>
2024-09-19 16:57:02 -0500

    # Created UNITTESTS
    ## TestConfigManagerInitialization
    
    ### Tests
    
    - test init
    - test config equals default
    - test config_dir exists
    - test config_file exists
    - test load_configuration loads default config
    - test save_configuration verifies safe of Path laden object

---
+++ config_manager/config_manager.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+from json import (dumps, loads)
+from pathlib import PosixPath as Path
+
+# _path_parameters are those properties that are `Path` objects in runtime
+_path_parameters = ('config_dir', 'config_file')
+
+
+def default_config():
+    dconf = {
+        "config_dir": Path.home().joinpath(".bw_config_manager"),
+        "config_file": Path.home().joinpath(".bw_config_manager").joinpath("config"),
+    }
+    return dconf
+
+
+class ConfigManager(object):
+
+    def __init__(self, config=default_config()) -> None:
+        self.config = config
+        # Save Config if None Exists
+        if not self.config["config_dir"].exists():
+            self.config["config_dir"].mkdir()
+        self.save_config()
+
+    def save_config(self, config=None) -> None:
+        if not config:
+            config = self.config
+        # Capture Path object of Config, before save_config_filter that will strip Path Properties
+        # save_file = config["config_file"]
+        config["config_file"].write_text(dumps(self.save_config_filter(config)))
+
+    def load_config(self, config=None) -> dict:
+        if not config:
+            config = self.config
+        return self.load_config_filter(self.load_config_filter(self.config["config_file"].read_text()))
+
+    def save_config_filter(self, config: dict) -> dict:
+        # We DO NOT USE self.config | We
+        new_config = {}
+        for prop in config:
+            if prop in _path_parameters:
+                new_config[prop] = str(config[prop])
+            else:
+                config[prop] = self.config[prop]
+        return new_config
+
+    def load_config_filter(self, config) -> dict:
+        new_config = {}
+        for prop in config:
+            if prop in _path_parameters:
+                new_config[prop] = Path(config[prop])
+        return new_config
+
+    def default_config(self):
+        return default_config()
+
+
+if __name__ == "__main__":
+    cfg_mgr = ConfigManager()
---
+++ test/test_config_manager.py
@@ -0,0 +1,54 @@
+from json import (loads)
+from pathlib import PosixPath as Path
+from unittest import TestCase, main
+
+from config_manager.config_manager import ConfigManager, default_config
+
+
+# # SETUP - Eliminate Existing Configuration File and Directory
+# if default_config.get("config_file").exists():
+#     default_config["config_file"].unlink()
+#
+# if default_config.get("config_dir").exists():
+#     default_config["config_dir"].rmdir()
+
+
+class TestConfigManager(TestCase):
+
+    # Setup - Start Each Test with CLEAN Config Directory
+    def setUp(self) -> None:
+        self.tcm = ConfigManager()
+
+    def tearDown(self) -> None:
+        if self.tcm.config["config_file"].exists():
+            self.tcm.config["config_file"].unlink()
+        if self.tcm.config["config_dir"].exists():
+            self.tcm.config["config_dir"].rmdir()
+        del self.tcm
+
+    def test_init(self):
+        self.assertIsNotNone(self.tcm, True)
+
+    def test_config_equals_default(self):
+        self.assertDictEqual(self.tcm.config, default_config())  # dd assertion here
+
+    def test_config_dir_exists(self):
+        self.assertTrue(self.tcm.config["config_dir"].exists())
+
+    def test_config_file_exists(self):
+        self.assertTrue(self.tcm.config["config_file"].exists())
+
+    def test_load_configuration(self):
+        self.assertDictEqual(
+            self.tcm.load_config_filter(loads(self.tcm.config["config_file"].read_text())),
+            default_config())
+
+    def test_save_configuration(self):
+        test_conf = {
+            "config_dir": Path.home().joinpath("ConfigManagerTestDir"),
+            "config_file": Path.home().joinpath("cm_conf"),
+        }
+
+
+if __name__ == '__main__':
+    main()
