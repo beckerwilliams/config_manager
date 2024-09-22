6745401b2ae166e19dd924a32163389a81f57e10
austinRon <becker.wiliams@gmail.com>
2024-09-22 00:48:57 -0500

    New Configuration Manager Refactored, 88% Code Coverage All Tests Successful * __init__ * test_file_creation, validate instance class * test_config_required_properties * test_config_required_values_not_none * test_config_type ~r

--- config_manager/config_manager.py
+++ config_manager/config_manager.py
@@ -2,72 +2,94 @@
 from json import (dumps, loads)
 from pathlib import PosixPath as Path
 
-# _path_parameters are those properties that are `Path` objects in runtime
-_def_dirname: str = '.bw_cfgmgr'
+# _path_parameters are those properties that become `Path` objects in runtime
+# NOTE: Supply ONLY the NAMES - Not the path in which they reside.
+#       The Config Directory will always be placed in User's HOME
+#
+# You can change either _def_dirname or _def_cfgname, Or supply your own config on __init__
+
+
+_def_dirname: str = '.bw_cfg'
 _def_cfgname: str = '.config'
-_default_pre_config: tuple = (
-    ('config_dir', _def_dirname),
-    ('config_file', '/'.join((_def_dirname, _def_cfgname)))
-)
 
-
-def _path_parameters() -> list:
-    return [entry[0] for entry in [entry for entry in _default_pre_config]]
+# ________________________________________ (DO NOT MODIFY BELOW) _________________________________
+_default_pre_config: tuple = (('config_dir', _def_dirname), ('config_file', _def_cfgname))
 
 
 def default_config() -> dict:
-    config = dict((name, Path.home().joinpath(value).absolute()) for name, value in _default_pre_config)
+    """
+
+    :return: JSON Object (No PATH Parameters as Values)
+    :rtype: dict
+    """
+    return dict((name, value) for name, value in _default_pre_config)
+
+
+def _path_parameters() -> list:
+    """
+
+    :return: names of configuration properties to be treated as paths
+    :rtype: list
+    """
+    return [entry[0] for entry in [entry for entry in _default_pre_config]]
+
+
+def _enable_path_properties(config: dict) -> dict:
+    """
+    Converts Config Manager File format to Runtime Configuration
+    :param config: Runtime Configuration
+    :type config: dict
+    :return: Configuration in File Format
+    :rtype: dict
+    """
+    new_config = dict()
+    for prop in config:
+        if prop in _path_parameters():
+            if prop == 'config_dir':
+                new_config['config_dir'] = Path.home().joinpath(config['config_dir'])
+            elif prop == 'config_file':
+                new_config['config_file'] = Path.home().joinpath(config['config_dir'], config['config_file'])
+        else:
+            new_config[prop] = config[prop]
+    return new_config
+
+
+def _disable_path_properties(config: dict) -> dict:
+    """
+    Converts Runtime Configuration to Configuration Manager File Format
+
+    :param config: Working Directory and Configuration File
+    :type config: dict
+    :return: String formatted Configuration File (JSON)
+    :rtype: dict
+    """
+    config['config_dir'] = config['config_dir'].name
+    config['config_file'] = config['config_file'].name
     return config
 
 
 class ConfigManager(object):
 
     @classmethod
-    def __init__(self, config: dict = None) -> None:
-        if not config:  # default config
-            self.config = default_config()
-        else:  # user config
-            self.config = config
+    def __init__(cls, config: dict = None) -> None:
+        """
+        If no config provided, default is created and assigned as `cls.config`
 
-        # Make sure config_dir exists
-        if not self.config["config_dir"].exists():
-            # Create Directory and default config
-            self.config["config_dir"].mkdir()
-            self.save_config()
+        If a Configuration File corresponding to a config  that exists,
+        it will take precedence over the provided configuration dict.
+
+        :param config: Application Configuration Directory and File
+        :type config: dict
+        """
+        if not config:
+            config = default_config()
+
+        config_file = Path.home().joinpath(config["config_dir"], config["config_file"])
+        if config_file.exists and config_file.is_file():
+            cls.config = _enable_path_properties(loads(config_file.read_text()))
         else:
-            # If config exists, Load it, Otherwise Save it
-                self.load_config()
-
-    @classmethod
-    def load_config(self, config=None) -> dict:
-        if not config:
-            config = self.config
-        self.config = self.load_config_filter(loads(config["config_file"].read_text()))
-
-    @staticmethod
-    def load_config_filter(config) -> dict:
-        new_config = {}
-        for prop in config:
-            if prop in _path_parameters():
-                new_config[prop] = Path(config[prop])
-            else:
-                new_config[prop] = config[prop]
-        return new_config
-
-    @classmethod
-    def save_config(self, config=None) -> None:
-        if not config:
-            config = self.config
-        # if not config["config_file"].exists():
-        #     config["config_file"].touch()
-        config["config_file"].write_text(dumps(self.save_config_filter(config)))
-
-    @staticmethod
-    def save_config_filter(config: dict) -> dict:
-        new_config = {}
-        for prop in config:
-            if prop in _path_parameters():
-                new_config[prop] = str(config[prop])
-            else:
-                new_config[prop] = config[prop]
-        return new_config
+            cls.config = _enable_path_properties(config)
+            if not cls.config["config_dir"].exists():
+                Path.home().joinpath(cls.config["config_dir"]).mkdir(exist_ok=True)
+            # Save Configuration File
+            config_file.write_text(dumps(config))
--- test/test_config_manager.py
+++ test/test_config_manager.py
@@ -1,59 +1,64 @@
-from json import (loads)
+from json import loads
 from pathlib import PosixPath as Path
 from unittest import TestCase
 
 from config_manager.config_manager import ConfigManager, default_config
 
 
+# noinspection PyTypeChecker
 class TestConfigManager(TestCase):
 
     # Setup - Start Each Test with CLEAN Config Directory
     @classmethod
-    def setUp(self) -> None:
-        self.user_conf = {
-            "config_dir": Path.home().joinpath("ConfigManagerTestDir"),
-            "config_file": Path.home().joinpath("ConfigManagerTestDir","cm_conf"),
-            "application_property_0": "application_value_0",
-            "application_property_1": "application_value_1",
-            "application_property_N": "application_value_N"
-        }
-        self.userConfigManager = ConfigManager(self.user_conf)
-        self.defaultConfigManager = ConfigManager()
+    def setUp(cls) -> None:
+        cls.defaultCM = ConfigManager()
+
+        cls.user_conf = {"config_dir": "ConfigManagerTestDir", "config_file": "cm_conf",
+                         "application_property_0": "application_value_0",
+                         "application_property_1": "application_value_1",
+                         "application_property_N": "application_value_N"}
+        cls.userCM = ConfigManager(cls.user_conf)
 
     @classmethod
-    def tearDown(self) -> None:
-        if self.defaultConfigManager.config["config_file"].exists():
-            self.defaultConfigManager.config["config_file"].unlink()
-        if self.defaultConfigManager.config["config_dir"].exists():
-            self.defaultConfigManager.config["config_dir"].rmdir()
+    def tearDown(cls) -> None:
+        for test_config in [default_config(), cls.user_conf]:
+            if Path.home().joinpath(test_config["config_dir"], test_config["config_file"]).exists():
+                Path.home().joinpath(test_config["config_dir"], test_config["config_file"]).unlink()
+                Path.home().joinpath(test_config["config_dir"]).rmdir()
 
-        del self.defaultConfigManager
-
-    def test_default_config(self):
-        print(default_config())
-        self.assertIsNotNone(default_config())
-
-    # Need Two Additional Test Cases for coverage,
-    # 1. Where 'self.config["config_file].exists() is FALSE
-    # 2. Where the CONFIG saved has ADD'L Properties (besides `dir` and `file`)
-    def test_config_file_DOESNT_EXIST(self):
-        pass
-        self.defaultConfigManager.config["config_file"].unlink()
-        self.assertIsNotNone(self.defaultConfigManager.config["config_file"])
-
-
-
-
-    # @classmethod
-    # def test_init_default(self):
-    #     self.assertIsNotNone(self.defaultConfigManager, True)
-    #     self.assertTrue(self.defaultConfigManager.config["config_dir"].exists())
-    #     self.assertTrue(self.defaultConfigManager.config["config_file"].exists())
-    #     print(f'{self.defaultConfigManager.config}')
     #
-    # @classmethod
-    # def test_init_user_conf(self):
-    #     self.assertIsNotNone(self.userConfigManager)
+    @classmethod
+    def test__init__(cls) -> None:
+        # cls.assertIsInstance(cls, cls.defaultCM, ConfigManager)
+        cls.assertIsInstance(cls, cls.userCM, ConfigManager)
 
+    @classmethod  #
+    def test_config_type(cls) -> None:
+        # cls.assertIsInstance(cls, cls.defaultCM.config, dict)
+        cls.assertIsInstance(cls, cls.userCM.config, dict)
 
+    @classmethod  #
+    def test_config_required_properties(cls) -> None:
+        cls.assertIn(cls, 'config_dir', cls.defaultCM.config)
+        cls.assertIn(cls, 'config_file', cls.defaultCM.config)
+        cls.assertIn(cls, 'config_dir', cls.userCM.config)
+        cls.assertIn(cls, 'config_file', cls.userCM.config)
 
+    #
+    # @classmethod  #
+    def test_config_required_values_not_none(cls) -> None:
+        cls.assertIsNotNone(cls.defaultCM.config['config_dir'])
+        cls.assertIsNotNone(cls.defaultCM.config['config_file'])
+        cls.assertIsNotNone(cls.userCM.config['config_dir'])
+        cls.assertIsNotNone(cls.userCM.config['config_file'])
+
+    def test_config_file_creation(cls) -> None:
+        cls.assertTrue(cls.defaultCM.config['config_file'].exists())
+        cls.assertTrue(cls.userCM.config['config_file'].exists())
+
+        for conf_file in [cls.userCM.config['config_file'], cls.defaultCM.config['config_file']]:
+            userConfig = loads(conf_file.read_text())
+            cls.assertIn('config_dir', userConfig)
+            cls.assertIn('config_file', userConfig)
+            cls.assertIsInstance(userConfig['config_dir'], str)
+            cls.assertIsInstance(userConfig['config_file'], str)
